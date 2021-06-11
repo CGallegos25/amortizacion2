dbf559a1ab80de190f37d7b21d6ff1a4c7bc558b
Cristian <crisisragallegos25@gmail.com>
2021-06-11 08:23:38 -0500

    Actualizacion de calculo

--- src/app/amortizacion/amortizacion.component.ts
+++ src/app/amortizacion/amortizacion.component.ts
@@ -9,45 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AmortizacionComponent implements OnInit {
 
-  public amortizacionModel:any;
-  public calculos = new Calculos(0,0,0,0,0,0);
-  public arrayLista:Array<Calculos> = [];
+  public amortizacionModel: any;
+  public calculos = new Calculos(0, 0, 0, 0, 0, 0);
+  public arrayLista: Array<Calculos> = [];
 
   constructor() {
-    this.amortizacionModel = new AmortizacionModel(0,0,20);
-   }
+    this.amortizacionModel = new AmortizacionModel(0, 0, 20);
+  }
 
   ngOnInit(): void {
 
   }
 
-  getFormAmortizacion(){
-      this.arrayLista = [];
-      console.log(this.amortizacionModel.monto);
-      console.log(this.amortizacionModel.plazo);
-      for(let i=0; i < this.amortizacionModel.plazo; i++){
+  getFormAmortizacion() {
+    this.arrayLista = [];
 
-      if(i == 0){
-        let calcularInteres:number = ((this.amortizacionModel.monto*this.amortizacionModel.tasa)/(this.amortizacionModel.plazo))/100;
-        let amortizacion:number = ((this.amortizacionModel.monto)/(this.amortizacionModel.plazo));
-        let pago:any =(calcularInteres+amortizacion);
-        const deudaFinal:number = (((this.amortizacionModel.monto)+(calcularInteres))/100)-pago;
-        this.arrayLista.push(new Calculos(i+1, this.amortizacionModel.monto, calcularInteres, amortizacion, pago, deudaFinal));
 
-      } else{
-              let calcularInteresN:number = ((this.arrayLista[i-1].deudaFinal*this.amortizacionModel.tasa)/(this.amortizacionModel.plazo))/100;
-              let amortizacionN:number = ((this.amortizacionModel.monto)/(this.amortizacionModel.plazo));
-              let pagoN:number = (calcularInteresN+amortizacionN);
-              let deudaFinalN:number = ((this.sumar(this.arrayLista[i-1].deudaFinal,calcularInteresN))-pagoN);
+    for (let i = 0; i < this.amortizacionModel.plazo; i++) {
 
-              this.arrayLista.push(new Calculos(i+1, this.arrayLista[i-1].deudaFinal, calcularInteresN, amortizacionN, pagoN, deudaFinalN));
+      if (i == 0) {
+
+        let calcularInteres: number = ((this.amortizacionModel.monto * this.amortizacionModel.tasa) / this.amortizacionModel.plazo) / 100;
+        let amortizacion: number = (this.amortizacionModel.monto / this.amortizacionModel.plazo);
+        let pago: any = Number(amortizacion) + calcularInteres;
+        let deudaFinal = (Number(this.amortizacionModel.monto) + calcularInteres) - pago;
+        this.arrayLista.push(new Calculos(i + 1, this.amortizacionModel.monto, calcularInteres, amortizacion, pago, deudaFinal));
+
+      } else {
+        let calcularInteresN: number = ((this.arrayLista[i - 1].deudaFinal * this.amortizacionModel.tasa) / this.amortizacionModel.plazo) / 10;
+        let amortizacionN: number = (this.amortizacionModel.monto) / (this.amortizacionModel.plazo);
+        let pagoN: number = (calcularInteresN + amortizacionN);
+        let deudaFinalN: number = (Number(this.arrayLista[i - 1].deudaFinal) + calcularInteresN) - pagoN; //this.sumar(this.arrayLista[i-1].deudaFinal,calcularInteresN))-pagoN);
+
+        this.arrayLista.push(new Calculos(i + 1, this.arrayLista[i - 1].deudaFinal, calcularInteresN, amortizacionN, pagoN, deudaFinalN));
       }
     }
 
- }
-
-  sumar(valor1:number, valor2:number){
-    return (valor1+valor2);
   }
 
+  sumar(valor1: number, valor2: number) {
+
+    return Number(valor1 + valor2);
+  }
 }
